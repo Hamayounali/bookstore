dd41fdba32b06e30a1166f40d54bad7ddc5b030d
Hamayoun <hamayounemail@gmail.com>
2022-08-19 22:49:19 +0430

    App.test.js updated

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Books from './Components/Books';
 
 test('renders learn react link', () => {
-  render(<App />);
+  // eslint-disable-next-line react/react-in-jsx-scope
+  render(<Books />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });

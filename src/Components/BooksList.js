2c757f10f1f090a2f5b2fe7e39cd9f7dbe3071a3
Hamayoun <hamayounemail@gmail.com>
2022-08-20 23:27:11 +0430

    Update BooksList.js

--- src/Components/BooksList.js
+++ src/Components/BooksList.js
@@ -2,20 +2,12 @@ import React from 'react';
 import Books from './Books';
 
 class getBooks extends React.PureComponent() {
-  state = {
-    BookList: [
-      {
-        id: 1,
-        title: 'Book Title 1',
-        author: 'Autor Name 1',
-      },
-      {
-        id: 1,
-        title: 'Book Title 2',
-        author: 'Autor Name 2',
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      BookList: [],
+    };
+  }
 
   render() {
     return (
